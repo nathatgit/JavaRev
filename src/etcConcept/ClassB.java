baf104a6e69d02c95495ce3f843f58dd91bdd44b
Raghuvendra Nath <nathnasha@yahoo.com>
2022-01-01 19:36:33 +0530

    adding const

--- src/etcConcept/ClassB.java
+++ src/etcConcept/ClassB.java
@@ -1,25 +1,21 @@
 package etcConcept;
 
-public class ClassB extends ClassA{
-	
-	
-	public ClassB(int j)
-	{
+public class ClassB extends ClassA {
+
+	public ClassB(int j) {
 		super(j);
-		System.out.println("Child Class const and value of j is now "+ j);
+		System.out.println("Child Class const and value of j is now " + j);
 	}
-	
-	public ClassB()
-	{
+
+	public ClassB() {
 		super();
 		System.out.println("Child Class const");
 	}
-	
-	public ClassB(int j, int i)
-	{
-		super(i,j);
-		System.out.println("Child Class const and value of j is now "+ j);
-		System.out.println("Child Class const and value of i is now "+ i);
+
+	public ClassB(int j, int i) {
+		super(i, j);
+		System.out.println("Child Class const and value of j is now " + j);
+		System.out.println("Child Class const and value of i is now " + i);
 
 	}
 
@@ -27,10 +23,9 @@ public class ClassB extends ClassA{
 		// TODO Auto-generated method stub
 
 		ClassB b1 = new ClassB(19);
-		
+
 		ClassB b2 = new ClassB();
 
-		
 		ClassB b3 = new ClassB(19, 33);
 
 	}
